43a1460b19c3ef6d193cab8dd72164ae35283df6
Zachary LaCore <zlacore12@gmail.com>
2025-02-03 00:37:43 -0600

    changed mongodb uri

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://newuser:newuser@cluster0.vlnmq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 export default mongoose.connection;
