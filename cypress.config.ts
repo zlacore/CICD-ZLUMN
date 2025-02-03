26e79dda9b7a5128e5de089a8ec56485ba7ba510
Zachary LaCore <zlacore12@gmail.com>
2025-02-03 01:13:26 -0600

    frustrated

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
